f1055ed49acf3be2d37bb92b233084bc64fa3cd1
tevqoon <jure@Second-Brain2.home>
2023-12-02 15:51:04 +0100

    Lattices

--- haskell/2.hs
+++ haskell/2.hs
@@ -1,70 +1,74 @@
 import Data.Char
 import Data.List.Split
-import qualified Data.Text as T
+import Algebra.Lattice
+import Numeric.Natural
 
-data Cubes = Cubes { red :: Integer, green :: Integer, blue :: Integer} deriving (Show, Eq)
+data Cubes = Cubes { red :: Natural, green :: Natural, blue :: Natural} deriving (Show, Eq)
 
+-- Pointwise addition forms a monoid
 instance Semigroup Cubes where
   (Cubes r1 g1 b1) <> (Cubes r2 g2 b2) = Cubes (r1 + r2) (g1 + g2) (b1 + b2)
 
 instance Monoid Cubes where
   mempty = Cubes 0 0 0
 
-instance Ord Cubes where
-  (Cubes r1 g1 b1) <= (Cubes r2 g2 b2) = r1 <= r2 && g1 <= g2 && b1 <= b2
+-- The componentwise maximum and minimum form a lattice
+instance Lattice Cubes where
+  (Cubes r1 g1 b1) \/ (Cubes r2 g2 b2) = Cubes (max r1 r2) (max g1 g2) (max b1 b2) -- join
+  (Cubes r1 g1 b1) /\ (Cubes r2 g2 b2) = Cubes (min r1 r2) (min g1 g2) (min b1 b2) -- meet
+  -- slightly annoying we can't siply define a join semilattice
 
-  -- Note: This definition means that max needs not be equal to
-  -- one of the old elements. This is the behavior we want here,
-  -- but does not match the expected interactions in Ord.
-  -- Is that a good thing? Idk.
-  max (Cubes r1 g1 b1) (Cubes r2 g2 b2) =
-    Cubes (max r1 r2) (max g1 g2) (max b1 b2)
+instance BoundedJoinSemiLattice Cubes where
+  bottom = mempty
 
-data Game = Game { id_num :: Integer, moves :: [Cubes] } deriving (Show)
+data Game = Game { id_num :: Natural, moves :: [Cubes] } deriving (Show)
 
--- From something like "Game 37"
-extractId :: String -> Integer
-extractId = read . dropWhile (not . isDigit)
-
+-- From something like " 10 blue"
 readColor :: String -> Cubes
-readColor pull = case splitOn " " pull of 
-                   (_:x:"red":[])   -> Cubes (read x) 0 0
-                   (_:x:"green":[]) -> Cubes 0 (read x) 0
-                   (_:x:"blue":[])  -> Cubes 0 0 (read x)
-                   _               -> error "Bad pattern in color matcher."
+readColor pull = case splitOn " " pull of
+  (_:value:color:[]) -> case color of
+                          "red"   -> Cubes (read value) 0 0
+                          "green" -> Cubes 0 (read value) 0
+                          "blue"  -> Cubes 0 0 (read value)
+                          _       -> error "Bad pattern in color matcher."
 
--- Each pull's colors converted into Cubes, then summed as cubes
+-- Each pull's colors converted into Cubes, then summed as cubes into a complete hand
 processCubes :: String -> Cubes
 processCubes = foldMap readColor . splitOn ","
 
+-- From something like "Game 37"
+extractId :: String -> Natural
+extractId = read . dropWhile (not . isDigit)
+
 -- Makes a game with the given id and a list of hands for that game
 lineToGame :: String -> Game
-lineToGame line = Game {id_num = extractId game_id, moves = map processCubes hands}
+lineToGame line = Game {id_num = extractId game_id,
+                        moves = map processCubes hands}
   where
     game_id : hands = splitOneOf ":;" line
 
--- Check that all of the pulls in a game are possible
+-- Check that all of the pulls in a game are possible - all the pulls must be geq to target
 checkGame :: Cubes -> Game -> Bool
-checkGame target = all (<= target) . moves
+checkGame target = all (flip joinLeq target) . moves
 
--- Sum the ids of possible games
-solver1 :: [Game] -> Integer
-solver1  = sum . map id_num . filter (checkGame (Cubes 12 13 14))
+-- Sum the ids of possible games - here the explicit target is used
+solver1 :: [Game] -> Natural
+solver1 = sum . map id_num . filter (checkGame (Cubes 12 13 14))
 
-power :: Cubes -> Integer
+power :: Cubes -> Natural
 power (Cubes r g b) = r * g * b
 
--- Taking advantage of the ill-defined Ord to fold each game into
--- componentwise maxima
-count2 :: Game -> Integer
-count2 = power . foldr max mempty . moves
+-- The semilatice structure allows a simple join (pointwise maximum)
+-- of all the moves of a game. Then simply sum the powers.
+solver2 :: [Game] -> Natural
+solver2 = sum . map (power . joins . moves)
 
-solver2 :: [Game] -> Integer
-solver2 = sum . map count2
+readFileLines :: FilePath -> IO [String]
+readFileLines = fmap lines . readFile
 
 main :: IO ()
 main = do
-  contents <- readFile "../inputs/day2.txt"
-  let games = map lineToGame $ lines contents
+  contents <- readFileLines "../inputs/day2.txt"
+  let games = map lineToGame contents
   print $ solver1 games
   print $ solver2 games
