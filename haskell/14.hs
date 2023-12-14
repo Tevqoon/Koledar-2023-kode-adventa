543939699727e7930544a02ddf541a491e6592f1
tevqoon <jure@Second-Brain2.home>
2023-12-14 17:26:34 +0100

    Yet faster set solution, but not fast enough

--- haskell/14.hs
+++ haskell/14.hs
@@ -1,44 +1,57 @@
 import Data.List
 import Debug.Trace
 import Data.Maybe
-import Data.Matrix
-import Data.Array
+-- import Data.Matrix
+import Data.Bifunctor (bimap)
+import Data.Set qualified as S
 import Data.Map.Strict qualified as M
 
-loadLine d l = d * (length $ filter (=='O') $ l)
+-- type Board = Matrix Char
+-- type Board = M.Map (Int, Int) Char
+type Coord = (Int, Int)
+type Dimensions = (Int, Int)
+data Board = Board {circles :: S.Set Coord, cubes :: S.Set Coord, dimensions :: Dimensions} deriving (Eq, Ord, Show)
 
-getLoad :: [String] -> Int
-getLoad xs = sum $ zipWith loadLine [1..] $ reverse xs
+inBounds :: Dimensions -> Coord -> Bool
+inBounds (xm, ym) (x, y) = 0 <= x && x < xm && 0 <= y && y < ym
 
-getLoadM :: Matrix Char -> Int
-getLoadM m = sum $ toList $ mapPos (\(x, y) v -> if v == 'O' then l - x + 1 else 0) m
-  where l = length $ toLists m
+isFree :: Coord -> Board -> Bool
+isFree pos (Board cis cus ds)
+  | not $ inBounds ds pos = False
+  | otherwise             = S.notMember pos cis && S.notMember pos cus
 
--- A safe get which takes in a pair instead of two values
-safeGet1 :: Matrix a -> (Int, Int) -> Maybe a
-safeGet1 m (x, y) = safeGet x y m
+parse :: [String] -> Board
+parse b = (Board (S.fromList circles) (S.fromList squares) dims)
+  where coords = [(c, (x, y)) | (l, x) <- zip b [0..], (c, y) <- zip l [0..], c /= '.']
+        (circles, squares) = bimap (map snd) (map snd) $ partition (\(c, _) -> c == 'O') coords
+        dims = bimap (succ . maximum) (succ . maximum) $ unzip $ map snd coords       
+
+getLoad :: Board -> Int
+getLoad (Board cis cus (xm, ym)) = S.foldr (\(x, y) s -> s + xm - x) 0 cis
 
 -- Fixed point starting with some value
 fix :: Eq a => (a -> a) -> a -> a
 fix = until =<< ((==) =<<)
 
 -- The direction is encoded as a pair
-rollOneM :: (Int, Int) -> Matrix Char -> Matrix Char
-rollOneM (x, y) board = mapPos new_state board
+rollOne :: Coord -> Board -> Board
+rollOne (x', y') b@(Board cis cus ds) = Board (S.map new_state cis) cus ds
   where
-    new_state (x', y') '.' | (board `safeGet1` (x' - x, y' - y)) == Just 'O' = 'O'
-    new_state (x', y') 'O' | (board `safeGet1` (x' + x, y' + y)) == Just '.' = '.'
-    new_state (x', y') symb = symb
+    new_state (x, y) | isFree (x' + x, y' + y) b = (x' + x, y' + y) 
+    new_state symb = symb
 
-roll dir board = fix (rollOneM dir) board
+-- roll :: Coord -> Board -> Board
+-- roll dir board = fix (rollOne dir) board
 
 rollNorth = roll (-1, 0)
 rollSouth = roll (1, 0)
 rollEast = roll (0, 1)
 rollWest = roll (0, -1)
     
-solver1 = getLoadM . rollNorth
+solver1 :: Board -> Int
+solver1 = getLoad . rollNorth 
 
+oneCycle :: Board -> Board
 oneCycle = rollEast . rollSouth . rollWest . rollNorth
   
 printList :: [String] -> IO ()
@@ -61,14 +74,14 @@ findFirstDuplicate lst = (org, rep)
 --       where foundmaybe = M.lookup x seen
         
 
-solver2 n l = getLoadM $ allCycles !! first_n
+solver2 n l = getLoad $ allCycles !! first_n
   where allCycles = iterate oneCycle l
         (org, rep) = findFirstDuplicate $ allCycles
         first_n = org + mod (n - org) (rep - org)
         
 main :: IO ()
 main = do
-  contents <- (fromLists . lines) <$> readFile "../inputs/day14.txt"
+  contents <- (parse . lines) <$> readFile "../inputs/day14.txt"
   print $ solver1 contents
   print $ solver2 1000000000 contents
   
