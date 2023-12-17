2e2ce7d81ba59811c867e9cdb8477937b7103a49
tevqoon <jure@Second-Brain2.home>
2023-12-17 16:49:03 +0100

    ugly 17

--- haskell/17.hs
+++ haskell/17.hs
@@ -1,21 +1,24 @@
-{-# LANGUAGE TemplateHaskell #-}
-import Data.Function.Memoize
 import Data.NumInstances.Tuple ()
-import Data.Maybe (isJust)
+import Data.Maybe (isJust, fromJust)
 import Data.List
+import Algorithm.Search
 import qualified Debug.Trace as T
-import qualified Data.Matrix as A
+import Data.Char (intToDigit)
+import Data.Array
 
 type Coord = (Int, Int)
-data Direction = N | S | E | W deriving (Show, Eq, Enum, Ord)
-deriveMemoizable ''Direction
+data Direction = N | S | E | W deriving (Show, Eq, Enum, Ord, Ix)
 
--- A safe get which takes in a pair instead of two values
-safeGet1 :: A.Matrix a -> Coord -> Maybe a
-safeGet1 m (x, y) = A.safeGet x y m
+type Board = Array Coord Int
 
-parse :: [String] -> A.Matrix Int
-parse = A.fromLists . (map . map) (read . pure)
+printBoard :: Board -> String
+printBoard b = intercalate "\n" $ [[intToDigit $ b ! (x, y) | y <- [y1..y2]] | x <- [x1..x2]]
+  where
+    ((x1, y1), (x2, y2)) = bounds b
+
+parse :: [String] -> Board
+parse ls = listArray ((1, 1), (mx, my)) $ concat $ (map . map) (read . pure) ls
+  where (mx, my) = (length ls, length $ head ls)
 
 toTuple :: Direction -> Coord
 toTuple N = (-1, 0)
@@ -29,34 +32,67 @@ revDir S = N
 revDir E = W
 revDir W = E
 
-heatLoss :: A.Matrix Int -> Int
-heatLoss board = minimum [memoized (1, 1) dir 3 | dir <- [S, E]]
+turn :: Direction -> [Direction]
+turn N = [W, E]
+turn S = [W, E]
+turn E = [N, S]
+turn W = [N, S]
+
+solver1 board = minimum $ map (fst <$>) [searchDown, searchRight]
   where
-    bounds = (A.nrows board, A.ncols board)
-    memoized = memoFix3 aux
+    searchDown = searcher ((1, 1), S, 3)
+    searchRight = searcher ((1, 1), E, 3)
+    
+    bbounds@(lbound, upbound) = bounds board
+    searcher = dijkstra neighbors
+               (\_ (pos, _, _) -> board ! pos)
+               (\(pos, _, _) -> pos == upbound)
 
     decrOrReset current next n
       | current == next = pred n
       | otherwise       = 3
 
-    aux :: (Coord -> Direction -> Int -> Int) -> Coord -> Direction -> Int -> Int
-    aux f pos dir n
-      | pos == bounds = board A.! pos
-      | otherwise = T.trace (show ()) $ board A.! pos + minimum losses
-      where removeCurrentP = if n == 0 then [dir] else []
-            nextDirs = ([N, S, E, W] \\ [revDir dir]) \\ removeCurrentP
-            losses = [f nextPos ddir (decrOrReset dir ddir n)
-                     | ddir <- nextDirs
-                     , let nextPos = (pos + toTuple ddir)
-                     , isJust $ safeGet1 board nextPos]
+    -- T.trace (show ((pos, dir, n), nextPoss))
+    neighbors (pos, dir, n) =  nextPoss
+      where keepGoing = if n /= 1 then [dir] else []
+            nextDirs = keepGoing ++ (turn dir)
+            nextPoss = [(nextPos, ddir, decrOrReset dir ddir n)
+                       | ddir <- nextDirs
+                       , let nextPos = (pos + toTuple ddir)
+                       , inRange bbounds nextPos]
 
+solver2 board = fst <$> search
+  where
+    search = searcher ((1, 1), N, 0)
+    
+    bbounds@(lbound, upbound) = bounds board
+    searcher = dijkstra neighbors
+               (\_ (pos, _, _) -> board ! pos)
+               (\(pos, _, n) -> pos == upbound && n <= 7)
+
+    decrOrReset current next n
+      | current == next = pred n
+      | otherwise       = 10
+
+    -- T.trace (show ((pos, dir, n), nextPoss)) $ 
+    neighbors (pos, dir, n) = nextPoss
+      where keepGoing = if n > 1 then [dir] else []
+            turnIf = if n <= 7 then turn dir else []
+            nextDirs = keepGoing ++ turnIf
+            nextPoss = [(nextPos, ddir, decrOrReset dir ddir n)
+                       | ddir <- nextDirs
+                       , let nextPos = (pos + toTuple ddir)
+                       , inRange bbounds nextPos]
+
+    
 main :: IO ()
 main = do
-  contents <- (parse . lines) <$> readFile "../inuts/day17.txt"
-  print 0
+  contents <- (parse . lines) <$> readFile "../inputs/day17.txt"
+  print $ solver1 contents
+  print $ solver2 contents
 
-tinyTest = parse ["12",
-                  "13"]
+tinyTest = parse ["111111",
+                  "111125"]
 
 test = parse ["2413432311323",
               "3215453535623",
@@ -71,3 +107,9 @@ test = parse ["2413432311323",
               "1224686865563",
               "2546548887735",
               "4322674655533"]
+
+test2 = parse ["111111111111",
+         "999999999991",
+         "999999999991",
+         "999999999991",
+         "999999999991"]
