e682981aec0d3fb8536ed3a631715f00e82e62c2
tevqoon <jure@Second-Brain2.home>
2023-12-19 23:19:48 +0100

    good sol

--- haskell/19.hs
+++ haskell/19.hs
@@ -1,60 +1,60 @@
-import Data.List
-import Data.Map.Strict (Map)
+{-# LANGUAGE TemplateHaskell #-}
+
+import Control.Lens (Lens', makeLenses, (^.), set)
+import Data.List (partition, mapAccumL)
 import Data.List.Split (splitOn)
-import Data.Bifunctor (bimap, second, first)
+import Data.Bifunctor (bimap, second)
 import Data.Char (isDigit)
-import Data.Maybe 
+import Data.Maybe (mapMaybe, catMaybes)
+import Data.Map.Strict (Map)
+import Data.Tuple (swap)
 import qualified Data.Map.Strict as M
 
+class FromLetter f g where
+  fromLetter :: Char -> Lens' f g
+
 -- We can either accept, reject, or pass to another workflow
-data Name = R | Name String | A deriving (Show, Eq, Ord)
+data Address = A | R | Address String deriving (Eq, Ord, Show)
 
 -- A comparator either filters some values or accepts all of them.
-data Comparator = Comparator {attr :: Char, op :: Char, treshold :: Int} | Default deriving (Show, Eq, Ord)
-
--- Each workflow is comprised of checkers, which have a comparator and a name.
-data Checker = Checker {comp :: Comparator, next :: Name} deriving (Show, Eq, Ord)
+data Checker = Checker {attr :: Char, opr :: Char, threshold :: Int, next :: Address} | Default Address
 
 -- A workflow is a name and a list of checkers. The last one is always Default, but this is not checked.
-type Workflow = (Name, [Checker])
+type Workflow = (Address, [Checker])
 
 -- Since we are using workflows out of order, it is convenient to store them in a map.
-type Workflows = Map Name [Checker]
+type Workflows = Map Address [Checker]
 
 -- Each part has four relevant fields.
-data Part = Part {x :: Int, m :: Int, a :: Int, s :: Int} deriving (Show, Eq, Ord)
+data Part = Part {_x :: Int, _m :: Int, _a :: Int, _s :: Int} deriving (Show, Eq, Ord)
 
--- A hacky way to get record type fields via the name of the field
-class Getters a b where
-  fromLetter :: Char -> (a -> b)
+makeLenses ''Part
 
--- Parts are an instance of this.
-instance Getters Part Int where
-  fromLetter :: Char -> (Part -> Int)
+instance FromLetter Part Int where
   fromLetter 'x' = x
   fromLetter 'm' = m
   fromLetter 'a' = a
   fromLetter 's' = s
 
--- Parses a given name into the Name type
-parseName :: String -> Name
+-- Parses a given name into the Address type
+parseName :: String -> Address
 parseName "A"  = A
 parseName "R"  = R
-parseName name = Name name
-
-parseComparator :: String -> Comparator
-parseComparator comp = Comparator attr op num
-    where attr : op : numstr = comp
-          num = read numstr :: Int
+parseName name = Address name
 
 parseChecker :: String -> Checker
-parseChecker check = case body of
-  [name]       -> Checker Default (parseName name)
-  [comp, name] -> Checker (parseComparator comp) (parseName name)
-  where body = splitOn ":" check
+parseChecker check = case splitOn ":" check of
+  [name]       -> Default (parseName name)
+  [comp, name] -> Checker attr opr num (parseName name)
+    where attr : opr : numstr = comp
+          num = read numstr :: Int
 
+opr' :: Char -> (Int -> Int -> Bool)
+opr' '<' = (<)
+opr' '>' = (>)
+         
 parseWorkflow :: String -> Workflow
-parseWorkflow workflow = (Name name, map parseChecker checkers)
+parseWorkflow workflow = (Address name, map parseChecker checkers)
   where
     [name, rest] = splitOn "{" workflow
     checkers = splitOn "," $ init rest
@@ -68,122 +68,95 @@ parse :: String -> (Workflows, [Part])
 parse = bimap (M.fromList . map parseWorkflow . lines) (map parsePart . lines) . tuple . splitOn "\n\n"
   where tuple = \[x, y] -> (x, y)
 
-opCharToOp :: Char -> (Int -> Int -> Bool)
-opCharToOp '<' = (<)
-opCharToOp '>' = (>)
+applyComparator :: Part -> Checker -> Maybe Address
+applyComparator part (Default next) = Just next
+applyComparator part (Checker attr opr threshold next) =
+  if (opr' opr) (part^.(fromLetter attr)) threshold then Just next else Nothing
 
-applyComparator :: Part -> Comparator -> Bool
-applyComparator _ Default = True
-applyComparator part (Comparator attr op treshold) = (opCharToOp op) (fromLetter attr $ part) treshold
-
-nextIfChecker :: Part -> Checker -> Maybe Name
-nextIfChecker part (Checker comp next) = if applyComparator part comp then Just next else Nothing
-
-nextUp :: Part -> [Checker] -> Name
-nextUp part = head . mapMaybe (nextIfChecker part)
+nextUp :: Part -> [Checker] -> Address
+nextUp part = head . mapMaybe (applyComparator part)
 
 checkPart :: Workflows -> Part -> Bool
-checkPart ws part = go (Name "in")
+checkPart ws part = go (Address "in")
   where
-    go :: Name -> Bool
+    go :: Address -> Bool
     go workflow = case result of
       A -> True
       R -> False
-      Name nextUp -> go (Name nextUp)
+      Address nextUp -> go (Address nextUp)
       where result = nextUp part $ ws M.! workflow
 
 -- Filters the parts for those that fit and sums their values
-solver1 workflows parts = sum . map (\(Part x m a s) -> x + m + a + s) . filter (checkPart workflows) $ parts
+solver1 :: Workflows -> [Part] -> Int
+solver1 workflows = sum . map (\(Part x m a s) -> x + m + a + s) . filter (checkPart workflows)
 
 type Range = (Int, Int)
-data PartRange = PartRange {xs :: Range, ms :: Range, as :: Range, ss :: Range} deriving (Show, Eq, Ord)
+data PartRange = PartRange {_xs :: Range, _ms :: Range, _as :: Range, _ss :: Range}
+  deriving (Show, Eq, Ord)
 
-instance Getters PartRange Range where
-  fromLetter :: Char -> (PartRange -> Range)
+makeLenses ''PartRange
+
+instance FromLetter PartRange Range where
+  fromLetter :: Char -> Lens' PartRange Range
   fromLetter 'x' = xs
   fromLetter 'm' = ms
   fromLetter 'a' = as
   fromLetter 's' = ss
 
--- The number of elements in a closed interval
-rangeLen :: Range -> Int
-rangeLen (a, b) = b - a + 1
-
--- The individual number of possibilities in a given range is the product of the possibilities of each of its ranges
 partRangePossibilities :: PartRange -> Int
-partRangePossibilities (PartRange xsr msr asr ssr) = product $ map rangeLen [xsr, msr, asr, ssr] 
+partRangePossibilities (PartRange xsr msr asr ssr) =
+  product $ map rangeLen [xsr, msr, asr, ssr]
+  where rangeLen (a, b) = b - a + 1
 
--- Warning: Stupid implementation ahead
--- Splits the range into those elements that fit the operation and those that don't. 
-splitRange :: Range -> Int -> Char -> (Maybe Range, Maybe Range)
-splitRange (a, b) treshold operation = case partition (\x -> (opCharToOp operation) x treshold) [a..b] of
-  ([], ys) -> (Nothing, Just (head ys, last ys))
-  (xs, []) -> (Just (head xs, last xs), Nothing)
-  (xs, ys) -> (Just (head xs, last xs), Just (head ys, last ys))
-
--- Warning: lens required ahead
--- Simply replaces one field with another
-replaceField :: PartRange -> Char -> Range -> PartRange
-replaceField (PartRange xsr msr asr ssr) 'x' nrange = PartRange nrange msr asr ssr
-replaceField (PartRange xsr msr asr ssr) 'm' nrange = PartRange xsr nrange asr ssr
-replaceField (PartRange xsr msr asr ssr) 'a' nrange = PartRange xsr msr nrange ssr
-replaceField (PartRange xsr msr asr ssr) 's' nrange = PartRange xsr msr asr nrange
-
--- Ugly updater which takes into account the possibility of an empty range
-updatePrange :: PartRange -> Char -> (Maybe Range, Maybe Range) -> (Maybe PartRange, Maybe PartRange)
-updatePrange part field (Nothing, Nothing) = (Nothing, Nothing)
-updatePrange part field (Just nleft, Nothing) = (Just $ replaceField part field nleft, Nothing)
-updatePrange part field (Nothing, Just nright) = (Nothing, Just $ replaceField part field nright)
-updatePrange part field (Just nleft, Just nright) = (Just $ replaceField part field nleft, Just $ replaceField part field nright)
-
--- To split a part we update by the relevant letter. Everything gets passed to the helper functions
-splitPart :: Maybe PartRange -> Comparator -> (Maybe PartRange, Maybe PartRange)
-splitPart Nothing _     = (Nothing, Nothing)
-splitPart (Just part) (Comparator attr op treshold) = updatePrange part attr newRanges
-  where attrRange = (fromLetter attr) part
-        newRanges = splitRange attrRange treshold op
-
--- Applying a checker means applying the comparator, routing those that fit, and passing the rest
-routePart :: Maybe PartRange -> Checker -> (Maybe PartRange, Maybe (Name, PartRange))
-routePart Nothing _ = (Nothing, Nothing)
-routePart (Just part) (Checker Default next) = (Nothing, Just (next, part))
-routePart part (Checker comp next) = (newRangeR, newRangeL >>= (\r -> Just (next, r)))
-  where (newRangeL, newRangeR) = splitPart part comp
-
--- How a part range goes through a given workflow
--- Fold each checker successively, accumulating the rest of the elements
--- by the end of the workflow, we have consumed all of the initial range.
-stepWorkflow :: PartRange -> [Checker] -> [(Name, PartRange)]
-stepWorkflow part checkers = catMaybes route
-  where (_, route) = mapAccumL routePart (Just part) checkers
-
--- A wrapper for stepWorkflow that takes the name of a workflow instead of its list of checkers
-stepWorkflows :: Workflows -> (Name, PartRange) -> [(Name, PartRange)]
-stepWorkflows ws (name, partRange) = stepWorkflow partRange checkers
-  where checkers = ws M.! name
-
--- Starts off with a range of all the possible values at the initial workflow, then on each step
--- monadically accumulates the next workflow step. Accepted elements at each step get rolled into
--- the sum of possibilities, and the rejected elements are discarded.
-solver2 :: Workflows -> Int
-solver2 ws = go [(Name "in", PartRange (1,4000) (1,4000) (1,4000) (1,4000))] 0
+splitRange :: Range -> Char -> Int -> (Maybe Range, Maybe Range)
+splitRange (a, b) opr threshold
+  | a > b = (Nothing, Nothing) -- Invalid range
+  | otherwise = case opr of
+      '>' -> (makeRange (max (threshold + 1) a) b, makeRange a (min threshold b))
+      '<' -> (makeRange a (min (threshold - 1) b), makeRange (max threshold a) b)
+      _   -> (Nothing, Nothing) -- Invalid operator
   where
-    go :: [(Name, PartRange)] -> Int -> Int
+    makeRange x y
+      | x > y     = Nothing
+      | otherwise = Just (x, y)
+
+updatePrange :: PartRange -> Char -> (Maybe Range, Maybe Range)
+  -> (Maybe PartRange, Maybe PartRange)
+updatePrange part field = bimap replacer replacer
+  where replacer = (>>= (\r -> Just $ set (fromLetter field) r part))
+
+routePart :: Maybe PartRange -> Checker
+  -> (Maybe PartRange, Maybe (Address, PartRange))
+routePart Nothing      _                      = (Nothing, Nothing)
+routePart (Just part) (Default next)          = (Nothing, Just (next, part))
+routePart (Just part) (Checker attr opr threshold next) = second (fmap (next,)) updated
+  where updated = updatePrange part attr $ newRanges
+        attrRange = part^.(fromLetter attr)
+        newRanges = swap $ splitRange attrRange opr threshold
+
+stepWorkflows :: Workflows -> (Address, PartRange) -> [(Address, PartRange)]
+stepWorkflows ws (name, part) = catMaybes route
+  where (_, route) = mapAccumL routePart (Just part) checkers
+        checkers = ws M.! name
+        
+solver2 :: Workflows -> Int
+solver2 ws = go [(Address "in", PartRange (1,4000) (1,4000) (1,4000) (1,4000))] 0
+  where
+    go :: [(Address, PartRange)] -> Int -> Int
     go [] n = n
-    go toRoute n = go rest2 (n + additionalPossibilities)
+    go toRoute n = go rest (n + additionalPossibilities)
       where upNext = toRoute >>= stepWorkflows ws
-            (rejected, rest1) = partition ((==R) . fst) upNext
-            (accepted, rest2) = partition ((==A) . fst) rest1
+            (accepted, rest) = partition ((==A) . fst) $ filter ((/=)R . fst) upNext
             additionalPossibilities = sum $ map partRangePossibilities $ map snd $ accepted
 
 main :: IO ()
 main = do
-  --(workflows, parts) <- parse <$> readFile "../inputs/test19.txt"
+  -- (workflows, parts) <- parse <$> readFile "../inputs/test19.txt"
   (workflows, parts) <- parse <$> readFile "../inputs/day19.txt"
   print $ solver1 workflows parts
   print $ solver2 workflows
 
 testW = "px{a<2006:qkq,m>2090:A,rfg}"
 (testN, testCheckers) = parseWorkflow testW
-testComp = Comparator {attr = 'a', op = '<', treshold = 2006}
+-- `testComp = Comparator {attr = 'a', fun = (<2006)}
 testPrange = PartRange (1,4000) (1,4000) (1,4000) (1,4000)
